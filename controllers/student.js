d8530aa95846759eeed9ba53ef3b9c4291771bb4
Raphelj <rapheljohnson.rap123@gmail.com>
2025-08-28 11:45:18 +0530

    all files added

---
+++ controllers/student.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const allStudents = (req,res)=>{
+    res.send("All students")
+}
+
+const createStudent = (req,res)=>{
+    res.send("Create student")
+}
+
+const updateStudent = (req,res)=>{
+    res.send("Update student")
+}
+
+const deleteStudent = (req,res)=>{
+    res.send("Delete student")
+}
+
+export {allStudents, createStudent, updateStudent, deleteStudent}
---
+++ routes/teachers.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/all', (req, res) => {
+    res.send("All Teachers")
+})
+router.post('/create', (req, res) => {
+    res.send("Sign in Teacher")
+})
+router.put('/update', (req, res) => {
+    res.send("Update Teacher")
+})
+router.delete('/delete', (req, res) => {
+    res.send("Remove Teachers")
+})
+
+export default router
+
